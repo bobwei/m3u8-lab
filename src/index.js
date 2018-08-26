d2880ef23ce49474f35980dc7ed8714329fd96d9
Bob Wei <bob.wei@linecorp.com>
2018-08-26 11:05:43 +0800

    Add spinner to display progress

--- src/index.js
+++ src/index.js
@@ -2,19 +2,28 @@
 import fs from 'fs';
 import path from 'path';
 import m3u8stream from 'm3u8stream';
+import ora from 'ora';
+import bytes from 'bytes';
 
 const fn = input => {
+  const spinner = ora('Loading...').start();
+  let totalDataLength = 0;
   const promises = input.map((data, i) => {
     const { url, name = i.toString() } = data;
     return new Promise(resolve => {
       const outputPath = path.join('./dist', `${name}.mp4`);
       console.log(`loading ${url} into ${outputPath}`);
       m3u8stream(url)
+        .on('data', chunk => {
+          totalDataLength += chunk.length;
+          const displayBytes = bytes(totalDataLength);
+          spinner.text = `${displayBytes} loaded.`;
+        })
         .pipe(fs.createWriteStream(outputPath))
         .on('end', () => resolve());
     });
   });
-  return Promise.all(promises);
+  return Promise.all(promises).then(() => spinner.succeed());
 };
 
 export default fn;
